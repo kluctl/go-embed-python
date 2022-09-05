224fe312d3114ae570dfbc6d7d86824d34a271b6
Alexander Block <ablock84@gmail.com>
2022-09-05 14:45:32 +0200

    fix: Fix embed patterns

--- internal/embed/embed_darwin_arm64.go
+++ internal/embed/embed_darwin_arm64.go
@@ -5,6 +5,6 @@ import (
 	"io/fs"
 )
 
-//go:embed all:./data/python-darwin-arm64
+//go:embed all:data/python-darwin-arm64
 var _pythonLib embed.FS
 var PythonLib, _ = fs.Sub(_pythonLib, "data/python-darwin-arm64")
--- internal/embed/embed_linux_amd64.go
+++ internal/embed/embed_linux_amd64.go
@@ -5,6 +5,6 @@ import (
 	"io/fs"
 )
 
-//go:embed all:./data/python-linux-amd64
+//go:embed all:data/python-linux-amd64
 var _pythonLib embed.FS
 var PythonLib, _ = fs.Sub(_pythonLib, "data/python-linux-amd64")
