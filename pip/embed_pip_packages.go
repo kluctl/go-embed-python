4a04fd590fe6a1056ac5981fb537cf622446aaeb
Alexander Block <ablock84@gmail.com>
2022-09-06 16:11:45 +0200

    Revert "fix: Fix host dependend python path in generated files"
    This reverts commit bf28a82ea9b120b1ff3582b08f335ee8a02b10b2.

--- pip/embed_pip_packages.go
+++ pip/embed_pip_packages.go
@@ -119,9 +119,5 @@ func pipInstall(ep *python.EmbeddedPython, requirementsFile string, platform str
 	if err != nil {
 		return err
 	}
-	err = internal.ReplaceStrings(targetDir, ep.GetBinPath()+string(os.PathSeparator), "")
-	if err != nil {
-		return err
-	}
 	return nil
 }
