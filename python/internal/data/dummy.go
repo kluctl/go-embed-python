5819f4161251fd9db63c5a66d2e7039eca7ad556
Alexander Block <ablock84@gmail.com>
2026-06-09 08:07:02 +0200

    chore: Fix typo

--- python/internal/data/dummy.go
+++ python/internal/data/dummy.go
@@ -1,6 +1,6 @@
 package data
 
 // PLEASE READ THIS!!!!
-// This file is really just a dummy. The release process will remove this file and generate some read embedded files
+// This file is really just a dummy. The release process will remove this file and generate some real embedded files
 // and commit these into a temporary branch and then tag it. This is to avoid clogging up the main branch with too many
 // binary files, which would be a very bad experience when pulling in go-embed-python as a dependency.
